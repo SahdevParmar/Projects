ea9a1c67e1fbf22cfd12e88442734797c59ab1c8
SahdevParmar <ronixmax199@gmail.com>
2026-08-12 21:24:29 +0530

    k

--- todo/script.js
+++ todo/script.js
@@ -1,13 +1,39 @@
+let tasksData={}
+
 const todo=document.querySelector('#todo')
 const progress=document.querySelector('#progress')
 const done=document.querySelector('#done')
+const columns=[todo,progress,done]
 let dragElement=null;
+
+if(localStorage.getItem("tasksData")){
+    const data =JSON.parse(localStorage.getItem("tasksData"));
+    console.log(data)
+}
+
+
 const tasks=document.querySelectorAll(".task");
 const addNewBtn=document.querySelector(".addNewBtn")
 const modal=document.querySelector(".modal")
 const submitNewTask=document.querySelector(".submitNewTask")
 
 
+function checkCount(){
+    columns.forEach(col=>{
+        const tasks=col.querySelectorAll(".task");
+        const count=col.querySelector(".right");
+
+        tasksData[col.id]=Array.from(tasks).map(t=>{
+            return {
+                heading: t.querySelector("h2").innerText,
+                description: t.querySelector("p").innerText,
+            }
+        })
+        localStorage.setItem("tasksData",JSON.stringify(tasksData))
+        count.innerText=tasks.length;
+        })
+}
+checkCount();
 
 function addDragEventsOnColumn(column){
     column.addEventListener("dragenter",(e)=>{
@@ -23,20 +49,13 @@ function addDragEventsOnColumn(column){
     })
     column.addEventListener("drop",(e)=>{
         column.querySelector('.task-list').appendChild(dragElement);
+        
+        checkCount();
     })
 
 }
 
-addDragEventsOnColumn(todo);
-addDragEventsOnColumn(progress);
-addDragEventsOnColumn(done);
-
-addNewBtn.addEventListener("click",(e)=>{
-    modal.style.display="flex";
-})
-submitNewTask.addEventListener("click",(e)=>{
-    e.preventDefault();
-    modal.style.display="none";
+function createTask(columnName){
     let t=document.createElement("div");
     t.classList.add("task");
     t.setAttribute("draggable","true")
@@ -54,11 +73,26 @@ submitNewTask.addEventListener("click",(e)=>{
     
     details.innerText=document.getElementById('taskDetails').value;
 
-    todo.querySelector('.task-list').appendChild(t);
+    columnName.querySelector('.task-list').appendChild(t);
     t.addEventListener("drag",(e)=>{
     dragElement=t;
     })
+}
+
+addDragEventsOnColumn(todo);
+addDragEventsOnColumn(progress);
+addDragEventsOnColumn(done);
+
+addNewBtn.addEventListener("click",(e)=>{
+    modal.style.display="flex";
 })
+submitNewTask.addEventListener("click",(e)=>{
+    e.preventDefault();
+    modal.style.display="none";
+    createTask(todo);
+    checkCount();
+})
+
 tasks.forEach(task=>{
     task.addEventListener("drag",(e)=>{
     dragElement=task;
