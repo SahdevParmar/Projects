8430db3f349432f63106075a9ca7ae304975d5a3
SahdevParmar <ronixmax199@gmail.com>
2026-08-12 14:10:34 +0530

    S

---
+++ todo/script.js
@@ -0,0 +1,22 @@
+const todo=document.querySelector('#todo')
+const progress=document.querySelector('#progress')
+const done=document.querySelector('#done')
+
+const tasks=document.querySelectorAll("drag",(e)=>{
+    
+})
+
+function addDragEventsOnColumn(column){
+    column.addEventListener("dragenter",(e)=>{
+        e.preventDefault();
+        column.classList.add("hover-over");
+    })
+    column.addEventListener("dragleave", (e)=>{
+        e.preventDefault();
+        column.classList.remove("hover-over");
+    })
+}
+
+addDragEventsOnColumn(todo);
+addDragEventsOnColumn(progress);
+addDragEventsOnColumn(done);
